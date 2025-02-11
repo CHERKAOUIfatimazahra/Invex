f2044e6e618aeb0193f37810aa277186f8099a46
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-11 11:37:13 +0100

    Refactor App component to implement navigation and add initial screens

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import StartScreen from "./screens/StartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import MainDashboardScreen from "./screens/MainDashboardScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const THEME = {
+  colors: {
+    primary: "#2E8B57",
+    black: "#000000",
+    white: "#FFFFFF",
+    gray: "#DDDDDD",
+    red: "#FF0000",
   },
-});
+  spacing: {
+    small: 10,
+    medium: 20,
+    large: 40,
+  },
+  sizes: {
+    icon: {
+      small: 24,
+      large: 30,
+    },
+    header: {
+      height: 70,
+    },
+  },
+};
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+        screenOptions={{
+          headerShown: false, // This will hide the header for all screens
+        }}
+      >
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen name="Login">
+          {(props) => <LoginScreen {...props} setIsLoggedIn={setIsLoggedIn} />}
+        </Stack.Screen>
+        <Stack.Screen name="MainDashboard" component={MainDashboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
