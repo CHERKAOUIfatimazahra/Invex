a70733e078051cc00314b187f62b52fa4740d2ee
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-02-13 11:43:19 +0100

    Wrap application in WarehouseProvider for context management

--- frontend/App.js
+++ frontend/App.js
@@ -10,65 +10,49 @@ import StatisticsScreen from "./screens/StatisticsScreen";
 import PDFScreen from "./screens/PDFScreen";
 import ProductDetailScreen from "./screens/ProductDetailScreen";
 import AddProductScreen from "./screens/AddProductScreen";
+import { WarehouseProvider } from "./Context/WarehouseContext";
 
 const Stack = createStackNavigator();
-
-const THEME = {
-  colors: {
-    primary: "#2E8B57",
-    black: "#000000",
-    white: "#FFFFFF",
-    gray: "#DDDDDD",
-    red: "#FF0000",
-  },
-  spacing: {
-    small: 10,
-    medium: 20,
-    large: 40,
-  },
-  sizes: {
-    icon: {
-      small: 24,
-      large: 30,
-    },
-    header: {
-      height: 70,
-    },
-  },
-};
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Start" component={StartScreen} />
-        <Stack.Screen name="Login">
-          {(props) => <LoginScreen {...props} setIsLoggedIn={setIsLoggedIn} />}
-        </Stack.Screen>
+    <WarehouseProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Start"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Start" component={StartScreen} />
+          <Stack.Screen name="Login">
+            {(props) => (
+              <LoginScreen {...props} setIsLoggedIn={setIsLoggedIn} />
+            )}
+          </Stack.Screen>
 
-        {isLoggedIn && (
-          <>
-            <Stack.Screen
-              name="MainDashboard"
-              component={MainDashboardScreen}
-            />
-            <Stack.Screen name="Scan" component={ScanScreen} />
-            <Stack.Screen name="Products" component={ProductsScreen} />
-            <Stack.Screen name="Statistics" component={StatisticsScreen} />
-            <Stack.Screen name="PDF" component={PDFScreen} />
-            <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
-            <Stack.Screen name="AddProduct" component={AddProductScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+          {isLoggedIn && (
+            <>
+              <Stack.Screen
+                name="MainDashboard"
+                component={MainDashboardScreen}
+              />
+              <Stack.Screen name="Scan" component={ScanScreen} />
+              <Stack.Screen name="Products" component={ProductsScreen} />
+              <Stack.Screen name="Statistics" component={StatisticsScreen} />
+              <Stack.Screen name="PDF" component={PDFScreen} />
+              <Stack.Screen
+                name="ProductDetail"
+                component={ProductDetailScreen}
+              />
+              <Stack.Screen name="AddProduct" component={AddProductScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </WarehouseProvider>
   );
 };
 
